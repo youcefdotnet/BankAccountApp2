4325cb9e3f163fe9faae874dfc170198b429c183
Youcef <youcef.dotnet@outlook.com>
2025-03-08 18:43:42 +0100

    nzw class savingaccount added

---
+++ SavingAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApp2
+{
+    public class SavingAccount: BankAccount
+    {
+        public decimal InterestRate { get; set; }
+
+        public SavingAccount(string owner, decimal interestrate): base(owner)
+        {
+            InterestRate = interestrate;
+        }
+    }
+}
